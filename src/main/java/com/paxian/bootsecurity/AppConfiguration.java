21d1f75361e284f5fa57b797bacc9446a744cf86
Pius <ppax1706@gmail.com>
2021-02-19 14:39:12 +0100

    initial commit

--- src/main/java/com/paxian/bootsecurity/AppConfiguration.java
+++ src/main/java/com/paxian/bootsecurity/AppConfiguration.java
@@ -12,14 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfiguration {
 
-    /*server.port=8443
-server.ssl.enabled=true
-server.ssl.key-store=src/main/resources/bootsecurity.p12
-server.ssl.key-store-password=bootsecurity
-server.ssl.keyStoreType=PKCS12
-server.ssl.keyAlias=bootsecurity*/
-
-    /*@Bean
+    @Bean
     public ServletWebServerFactory servletContainer() {
         // Enable SSL Trafic
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
@@ -40,11 +33,11 @@ server.ssl.keyAlias=bootsecurity*/
         return tomcat;
     }
 
-    *//*
+    /*
     We need to redirect from HTTP to HTTPS. Without SSL, this application used
     port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
     redirected to HTTPS on 8443.
-     *//*
+     */
     private Connector httpToHttpsRedirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
@@ -52,5 +45,5 @@ server.ssl.keyAlias=bootsecurity*/
         connector.setSecure(false);
         connector.setRedirectPort(8443);
         return connector;
-    }*/
+    }
 }
